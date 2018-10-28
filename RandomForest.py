79b771c0b4fcec316f87d20c3f94b88654773ed4
Susheel Nath <naths@tcd.ie>
2018-10-28 20:39:23 +0000

    Update RandomForest.py
    Addition of comments

--- RandomForest.py
+++ RandomForest.py
@@ -1,16 +1,19 @@
 import pandas as pd 
+
 import numpy as np 
 
-data = pd.read_csv('KS_pre_process1.csv') 
+
+
+data = pd.read_csv('KS_pre_process1.csv') # Reading the dataset from the drive
 data.head()
-columns_target = ['state'] 
-columns_train = ['category', 'main_category', 'currency', 'goal', 'country', 'duration_days'] 
-data = data.dropna()
-data = data[(data['goal'] <= 100000) & (data['goal'] >= 1000)].copy()
+columns_target = ['state'] # target variable - state (Failures, Success)
+columns_train = ['category', 'main_category', 'currency', 'goal', 'country', 'duration_days'] # training variables
+data = data.dropna() # dropping all null values
 
 X = data[columns_train] 
 Y = data[columns_target]
 
+# Counting all null values to confirm
 X['category'].isnull().sum() 
 X['main_category'].isnull().sum()
 X['currency'].isnull().sum() 
@@ -23,6 +26,9 @@ X['main_category'].nunique()
 X['currency'].nunique() 
 X['country'].nunique() 
 X.head() 
+
+# Conversion of string to int
+
 X['category'] = pd.factorize(X.category)[0] + 1 
 X.head()
 X['main_category'] = pd.factorize(X.main_category)[0] + 1 
@@ -31,6 +37,8 @@ X['country'] = pd.factorize(X.country)[0] + 1
 X['main_category'] = pd.factorize(X.main_category)[0] + 1 
 X['currency'] = pd.factorize(X.currency)[0] + 1 
 X['country'] = pd.factorize(X.country)[0] + 1 
+
+# Performing RandomForest Classifier on the Dataset
 
 from sklearn.model_selection import train_test_split 
 X_train, X_test, Y_train, Y_test = train_test_split(X,Y, test_size = 0.20, random_state = 42) 
